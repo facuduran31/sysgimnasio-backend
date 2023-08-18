46872e6fa8207f4bb0537ce839e3b2bf6f197df4
facu <facundoduran31@gmail.com>
2023-08-18 15:57:49 -0300

    Ruta para validar token

--- app.js
+++ app.js
@@ -39,6 +39,22 @@ app.use('/api/usuarios', verificarToken, routerUsuarios);
 
 app.use('/api/planes', verificarToken, routerPlanes);
 
+app.get('/api/check-token', (req, res) => {
+    const token = req.header('Authorization');
+  
+    if (!token) {
+      return res.status(401).json(false); // No hay token, no es válido
+    }
+  
+    jwt.verify(token.split(' ')[1], SECRET_KEY, (err) => {
+      if (err) {
+        return res.status(200).json(false); // Token inválido
+      }
+  
+      return res.status(200).json(true); // Token válido
+    });
+  });
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
